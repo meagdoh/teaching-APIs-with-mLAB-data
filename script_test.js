a824c5d6fe63bd3143b7dcb39bd2329d237542ab
meagdoh <meagdoherty@gmail.com>
2017-09-18 11:17:00 -0400

    Passing AJAX test

--- script_test.js
+++ script_test.js
@@ -1,19 +1,15 @@
-var expect = chai.expect;
-
 chai.should();
 
-var mySpy = sinon.spy(jQuery, "ajax");
+describe("Test API", function() {
 
+  it('executes a GET request for ISPs', function() {
+    var mySpy = sinon.spy(jQuery, "ajax");
+    mySpy.calledWithMatch({
+      url: 'http://data-api.measurementlab.net/locations/nausdcwashington/clients'
+    });
+  });
+  after(function() {
+    jQuery.ajax.restore();
+  });
 
-it('executes a GET request for ISPs', function () {
-    sinon.stub(jQuery, 'ajax');
-
-    sinon.assert.should.always.have.been.calledWithMatch({ url: 'http://data-api.measurementlab.net/locations/nausdcwashington/clients' });
-});
-
-
-after(function () {
-  // When the test either fails or passes, restore the original
-  // jQuery ajax function
-  jQuery.ajax.restore();
 });
