774c2435e490c7f8d0d56261ed1b7bcf3a5f8e4f
meagdoh <meagdoherty@gmail.com>
2017-09-20 15:54:50 -0400

    add City Name input field and location search AJAX call

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 $(document).ready(() => {
-  $("#button").on("click", () => {
-    let url = "http://data-api.measurementlab.net/locations/nausdcwashington/clients"
+  $('#button').click(function() {
+
+    var cityName = $("input").val()
+
+    let url = 'http://data-api.measurementlab.net/locations/search?q=' + cityName
     $.ajax({
       url,
       type: "get",
       dataType: "json"
     }).done((response) => {
 
-      for(i = 0; i < 4; i++){
-      console.log(response.results[i].meta.client_asn_name)
+      for (i = 0; i < 4; i++) {
+        console.log(response.results[i].meta.client_city + ", " + response.results[i].meta.client_region )
 
-      let list =   $(`<li>${response.results[i].meta.client_asn_name}</li>`)
-      $("#body").append(list)
-    };
+        let list = $(`<li>${response.results[i].meta.client_city + ", " + response.results[i].meta.client_region}</li>`)
+        $("#body").append(list)
+      };
     }).fail(() => {
       console.log("ajax request fails")
     }).always(() => {
       // console.log(" this always happens regardless of sucess or failure")
     })
-  })
+  });
 });
+//   $("#button").on("click", () => {
+//     let url = 'http://data-api.measurementlab.net/locations/search?q='+cityName
+//     $.ajax({
+//       url,
+//       type: "get",
+//       dataType: "json"
+//     }).done((response) => {
+//
+//       for (i = 0; i < 4; i++) {
+//         console.log(response.results[i].meta.client_asn_name)
+//
+//         let list = $(`<li>${response.results[i].meta.client_asn_name}</li>`)
+//         $("#body").append(list)
+//       };
+//     }).fail(() => {
+//       console.log("ajax request fails")
+//     }).always(() => {
+//       // console.log(" this always happens regardless of sucess or failure")
+//     })
+//   })
+// });
