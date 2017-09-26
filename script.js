dbffec7a7cef33e78d5f41709e2b074c08a8d95f
meagdoh <meagdoherty@gmail.com>
2017-09-26 15:36:20 -0400

    add jQueryUI and replace autocomplete with DevBridge

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 $(document).ready(() => {
-  $('#button').click(function() {
 
-    var cityName = $("input").val()
-
-    let url = 'http://data-api.measurementlab.net/locations/search?q=' + cityName
-    $.ajax({
-      url,
-      type: "get",
-      dataType: "json"
-    }).done((response) => {
-
-      for (i = 0; i < 4; i++) {
-        console.log(response.results[i].meta.client_city + ", " + response.results[i].meta.client_region )
-
-        let list = $(`<li>${response.results[i].meta.client_city + ", " + response.results[i].meta.client_region}</li>`)
-        $("#body").append(list)
-      };
-    }).fail(() => {
-      console.log("ajax request fails")
-    }).always(() => {
-      // console.log(" this always happens regardless of sucess or failure")
-    })
+  $('#auto-Complete').autocomplete({
+      serviceURL: 'http://data-api.measurementlab.net/locations/search?q=',
+      onSelect: function (suggestion) {
+              console.log('You selected: ' + suggestion.value + ', ' + suggestion.data);
+          }
   });
 });
+
+
+
+
+//   $('#button').click(function() {
+//
+//     var cityName = $("input").val()
+//
+//     let url = 'http://data-api.measurementlab.net/locations/search?q=' + cityName
+//     $.ajax({
+//       url,
+//       type: "get",
+//       dataType: "json"
+//     }).done((response) => {
+//
+//       for (i = 0; i < 4; i++) {
+//         console.log(response.results[i].meta.client_city + ", " + response.results[i].meta.client_region )
+//
+//         let list = $(`<li>${response.results[i].meta.client_city + ", " + response.results[i].meta.client_region}</li>`)
+//         $("#body").append(list)
+//       };
+//     }).fail(() => {
+//       console.log("ajax request fails")
+//     }).always(() => {
+//       // console.log(" this always happens regardless of sucess or failure")
+//     })
+//   });
+// });
 //   $("#button").on("click", () => {
 //     let url = 'http://data-api.measurementlab.net/locations/search?q='+cityName
 //     $.ajax({
